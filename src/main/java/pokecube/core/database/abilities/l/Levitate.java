b002c138ed5ca233fb3d583ffb2f8267fa44ae9e
Thutmose <elpatricimo@gmail.com>
2019-04-22 19:10:45 -0400

    Update Levitate.java
    Makes sure levitate properly applies when checking move damage for AI purposes

--- src/main/java/pokecube/core/database/abilities/l/Levitate.java
+++ src/main/java/pokecube/core/database/abilities/l/Levitate.java
@@ -3,7 +3,6 @@ package pokecube.core.database.abilities.l;
 import pokecube.core.database.abilities.Ability;
 import pokecube.core.interfaces.IPokemob;
 import pokecube.core.interfaces.IPokemob.MovePacket;
-import pokecube.core.interfaces.Move_Base;
 import pokecube.core.utils.PokeType;
 
 public class Levitate extends Ability
@@ -11,9 +10,14 @@ public class Levitate extends Ability
     @Override
     public void onMoveUse(IPokemob mob, MovePacket move)
     {
-        Move_Base attack = move.getMove();
-        IPokemob attacker = move.attacker;
-        if (attacker == mob || !move.pre || attacker == move.attacked) return;
-        if (attack.getType(move.attacker) == PokeType.getType("ground")) move.canceled = true;
+        if (move.attacker == mob || !move.pre || move.attacker == move.attacked) return;
+        if (move.getMove().getType(move.attacker) == PokeType.getType("ground")) move.canceled = true;
+    }
+
+    @Override
+    public int beforeDamage(IPokemob mob, MovePacket move, int damage)
+    {
+        if (move.getMove().getType(move.attacker) == PokeType.getType("ground")) return 0;
+        return super.beforeDamage(mob, move, damage);
     }
 }
